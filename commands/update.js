fc6fbdaf77bb0e1c11350e226bd5a80134d1fa26
WeiKuOuO <43096905+WeiKuOuO@users.noreply.github.com>
2018-11-04 23:22:45 +0800

    Update commands/update.js

--- commands/update.js
+++ commands/update.js
@@ -9,11 +9,12 @@ module.exports.run = async (bot, message, args) => {
     if(args.join(' ').length > maxLen) return message.channel.send(`你發了超過** ${maxLen} **個字!`).then(message => message.delete(5000)); 
   
     if(!args[0]) return message.channel.send('請在指令後方填上你要打的字...').then(message => message.delete(5000));
+    if(!args[1]) return message.channel.send('請在指令後方填上你要打的字...').then(message => message.delete(5000));
     let m = new Discord.RichEmbed()
         .setAuthor(bot.user.username)
         .setTitle("**最新更新**")
         .setColor(0x00D4FF)
-        .addField(args[0])
+        .addField(args[0],args[1])
         .setTimestamp(new Date())
         .setFooter("WeiKuOuO","https://avatars1.githubusercontent.com/u/43096905?s=400&u=264c38ae1fe19184e491b8fdbcdca8bea00e1612&v=4")
     bot.channels.get("508661180093693973").send(m)
