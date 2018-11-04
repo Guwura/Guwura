8c556aec7b5bc3081cdb441a959540bc06b13237
WeiKuOuO <43096905+WeiKuOuO@users.noreply.github.com>
2018-11-04 23:20:48 +0800

    Update commands/update.js

--- commands/update.js
+++ commands/update.js
@@ -15,7 +15,7 @@ module.exports.run = async (bot, message, args) => {
         .setTitle("**最新更新**")
         .setColor(0x00D4FF)
         .addField(args[0])
-        .setTimestamp(new Data())
+        .setTimestamp(new Date())
         .setFooter("WeiKuOuO","https://avatars1.githubusercontent.com/u/43096905?s=400&u=264c38ae1fe19184e491b8fdbcdca8bea00e1612&v=4")
     bot.channels.get("508661180093693973").send(m)
     bot.channels.get("507175076412784650").send(m)
