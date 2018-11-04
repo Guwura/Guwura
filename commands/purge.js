9c09ce97bc5a01cbf17397cb7a046ce844b97217
WeiKuOuO <43096905+WeiKuOuO@users.noreply.github.com>
2018-11-04 21:20:45 +0800

    Update commands/purge.js

--- commands/purge.js
+++ commands/purge.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js');
 module.exports.run = async (bot, message, args) => {
 
     if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send('抱歉,你沒有**管理員權限**來使用這個指令').then(message => message.delete(5000));
-    if (!message.guild.member(client.user).hasPermission("MANAGE_MESSAGES")) return message.channel.send('我沒有**管理員權限**來刪除訊息').then(message => message.delete(5000));
+    if (!message.guild.member(bot.user).hasPermission("MANAGE_MESSAGES")) return message.channel.send('我沒有**管理員權限**來刪除訊息').then(message => message.delete(5000));
 
     if (!args[0]) return message.channel.send('請填一個**1~100**的數字').then(message => message.delete(5000));
     if (args[0] < 1) return message.channel.send('請填一個**1~100**的數字').then(message => message.delete(5000));
