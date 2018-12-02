1c1c05628953a12e1e8faf58a772c8d74e7b1a15
WeiKuOuO <43096905+WeiKuOuO@users.noreply.github.com>
2018-12-02 20:12:03 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 const Discord = require('discord.js');
 const fs = require("fs");
 const { version } = require("discord.js");
@@ -14,32 +15,47 @@ const bot = new Discord.Client();
 const token = process.env.token
 const prefix = process.env.prefix
 
-let index = 0;
-
 bot.commands = new Discord.Collection();
 
 bot.on('ready', function() {
-      const statuslist = [
-          `muhc/help | 任何問題請WeiKu#3402 ♪`,
-          `機器人製作 | 微苦 ♪`,
-          `頭像繪製 | 星亞 ♪`,
-          `官方網站 | Muhc.tw ♪`,
-          `邀請Kizinn | Muhc.tw/bot ♪`,
-          `幫Kizinn按讚 | Muhc.tw/vote ♪`,
-          `Muhc官方頻道 | Muhc.tw/yt ♪`,
-          `Muhc官方群組 | Muhc.tw/dc ♪`,
-          `如想在你的伺服器使用kizinn-info請洽WeiKu#3402 ♪`
-      ];
-      bot.setInterval(() => {
-        bot.user.setActivity(statuslist[index], { type: "STREAMING", url: "https://www.twitch.tv/weikuouo"});
-        index++
-        if (index === statuslist.length) index = 0;
-    }, 3000)
+  const statuslist = [
+      `muhc/help | 任何問題請WeiKu#3402 ♪`,
+      `機器人製作 | 微苦 ♪`,
+      `頭像繪製 | 星亞 ♪`,
+      `官方網站 | Muhc.tw ♪`,
+      `邀請Kizinn | Muhc.tw/bot ♪`,
+      `幫Kizinn按讚 | Muhc.tw/vote ♪`,
+      `Muhc官方頻道 | Muhc.tw/yt ♪`,
+      `Muhc官方群組 | Muhc.tw/dc ♪`,
+      `如想在你的伺服器使用kizinn-info請洽WeiKu#3402 ♪`
+  ];
+  bot.setInterval(() => {
+    bot.user.setActivity(statuslist[index], { type: "STREAMING", url: "https://www.twitch.tv/weikuouo"});
+    index++
+    if (index === statuslist.length) index = 0;
+}, 3000)
 });
+
+bot.on('message', async message => {
+  if (message.author.bot) return
+  if (message.channel.id == "411894866222514188") {
+    if (message.content === "我同意") {
+      if (message.member.roles.has("411897336621432832")) {
+          message.channel.send("你已經同意了").then(message => message.delete(5000))
+          message.delete()
+      } else {
+          message.member.addRole('411897336621432832').then(message.channel.send("已給予身分組").then(message => message.delete(5000)))
+          message.delete()
+      }
+    } else {
+      message.channel.send("請輸入\"我同意\"").then(message => message.delete(5000));
+      message.delete()
+    }
+  }
+})
+
 bot.on("ready", async () => {
-
   console.log(`${bot.user.username}成功啟動了!^w^, [ ${bot.guilds.size} | ${bot.channels.size} | ${bot.users.size} ]`);
-
   bot.channels.get("508653447164329996").bulkDelete("50")
   bot.channels.get("507175036092940299").bulkDelete("50")
   bot.channels.get("518054671286534190").bulkDelete("50")
@@ -55,11 +71,10 @@ bot.on("ready", async () => {
       .addField(":green_book: Node.js版本", `\`\`\`diff\n- 偵測中...\`\`\``, true)
       .addField(":gear: CPU", `\`\`\`css\n偵測中...\`\`\``)
       .addField(":pager: CPU 使用率", `\`\`\`fix\n正在啟動...\`\`\``, true)
+      .addField(":orange_book: 位元數", `\`\`\`fix\n正在啟動...\`\`\``, true)
       .addField(":triangular_flag_on_post: 主機平台", `\`\`\`fix\n正在啟動...\`\`\``, true)
-      .addField(":ping_pong: Ping", `\`\`\`xl\n偵測中...\`\`\``, true)
-      .addField("**相關連結**",`\`\`\`diff\n+ Discord邀請連結 - https://muhc.tw/dc \n- 官方網站 - https://muhc.tw \n+ 幫機器人按讚 - https://muhc.tw/vote \`\`\``)
-      .addField("目前狀態","Vote數量")
-      .setImage("https://discordbots.org/api/widget/506843065424543745.png")
+      .addField(":ping_pong: Ping", `\`\`\`xl\n偵測中...\`\`\``)
+      .addField("**相關連結**",`\`\`\`diff\n+ Discord邀請連結 - https://muhc.tw/dc \n- 官方網站 - https://muhc.tw \`\`\``)
   let m1 = await bot.channels.get("508653447164329996").send(botstartinfo)
   let m2 = await bot.channels.get("507175036092940299").send(botstartinfo)
   let m3 = await bot.channels.get("518054671286534190").send(botstartinfo)
@@ -81,17 +96,17 @@ bot.on("ready", async () => {
         .addField(":green_book: Node.js版本", `\`\`\`diff\n- ${process.version}\`\`\``, true)
         .addField(":gear: CPU", `\`\`\`css\nIntel(R) Xeon(R) CPU E7-2860 v4 @ 2.26GHz\`\`\``)
         .addField(":pager: CPU 使用率", `\`\`\`fix\n${((((Math.random() * 5) + 1) / 5) * 6).toFixed(2)}%\`\`\``, true)
+        .addField(":orange_book: 位元數", `\`\`\`fix\n${os.arch()}\`\`\``, true)
         .addField(":triangular_flag_on_post: 主機平台", `\`\`\`fix\n${os.platform()}\`\`\``, true)
-        .addField(":ping_pong: Ping", `\`\`\`xl\n${Math.round(bot.ping)} ms\`\`\``, true) 
+        .addField(":ping_pong: Ping", `\`\`\`xl\n${Math.round(bot.ping)} ms\`\`\``) 
         .addField("**相關連結**",`\`\`\`diff\n+ Discord邀請連結 - https://muhc.tw/dc \n- 官方網站 - https://muhc.tw \n+ 幫機器人按讚 - https://muhc.tw/vote \`\`\``)
-        .addField("目前狀態","Vote數量")
-        .setImage("https://discordbots.org/api/widget/506843065424543745.png")
     m1.edit(botinfo)
     m2.edit(botinfo)
     m3.edit(botinfo)
     });
    }, 2200);
   })
+
 
 fs.readdir("./commands/", (err,files) => {
   if(err) console.log(err);
@@ -113,7 +128,7 @@ bot.on("message", async message => {
   //command handler
 	if (message.author.bot || message.channel.type === 'dm') return;
 	if (message.content.toLowerCase().indexOf(prefix) !== 0) return
-    const args = message.content.split(" ").slice(0);
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
     const command = args.shift().toLowerCase();
 	try{
     let commandFile = require(`./commands/${command}.js`);
@@ -124,22 +139,6 @@ bot.on("message", async message => {
   if(message.author.bot) return;
   if(message.content.indexOf(prefix) !== 0) return;
 
-//on r18
-if (message.author.bot) return
-  if (message.channel.id === "411894866222514188") {
-    if (message.content === "我同意") {
-      if (message.member.roles.has("411897336621432832")) {
-          message.channel.send("你已經同意了").then(message => message.delete(5000))
-          message.delete()
-      } else {
-          message.member.addRole('411897336621432832').then(message.channel.send("已給予身分組").then(message => message.delete(5000)))
-          message.delete()
-      }
-    } else {
-      message.channel.send("請輸入\"我同意\"").then(message => message.delete(5000));
-      message.delete()
-    }
-  }
 })
 
 
@@ -150,5 +149,6 @@ bot.on("guildCreate", guild => {
 bot.on("guildDelete", guild => {
   console.log(`退出群組 ${guild.name} [ ${guild.memberCount} ] (id: ${guild.id})`);
 });
+
 
 bot.login(token);
