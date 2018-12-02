cd25b2557e171aa48085d5705462d27b07638d72
WeiKuOuO <43096905+WeiKuOuO@users.noreply.github.com>
2018-12-02 13:58:32 +0800

    .

--- index.js
+++ index.js
@@ -38,10 +38,10 @@ bot.on('ready', function() {
 bot.on("ready", async () => {
 
   console.log(`${bot.user.username}成功啟動了!^w^, [ ${bot.guilds.size} | ${bot.channels.size} | ${bot.users.size} ]`);
-
-  bot.channels.get("508653447164329996").bulkDelete("50")
-  bot.channels.get("507175036092940299").bulkDelete("50")
-  bot.channels.get("518054671286534190").bulkDelete("50")
+  bot.channels.filter(c => c.name=="kizinn-info").forEach(c => c.bulkDelete("50"))
+  // bot.channels.get("508653447164329996").bulkDelete("50")
+  // bot.channels.get("507175036092940299").bulkDelete("50")
+  // bot.channels.get("518054671286534190").bulkDelete("50")
   const botstartinfo = new Discord.RichEmbed()
       .setAuthor(bot.user.username)
       .setTitle("**Bot資訊**")
@@ -56,9 +56,6 @@ bot.on("ready", async () => {
       .addField(":pager: CPU 使用率", `\`\`\`fix\n正在啟動...\`\`\``, true)
       .addField(":triangular_flag_on_post: 主機平台", `\`\`\`fix\n正在啟動...\`\`\``, true)
       .addField(":ping_pong: Ping", `\`\`\`xl\n偵測中...\`\`\``, true)
-      .addField("**相關連結**",`\`\`\`diff\n+ Discord邀請連結 - https://muhc.tw/dc \n- 官方網站 - https://muhc.tw \n+ 幫機器人按讚 - https://muhc.tw/vote \`\`\``)
-      .addField("目前狀態","Vote數量")
-      .setImage("https://discordbots.org/api/widget/506843065424543745.png")
   let m1 = await bot.channels.get("508653447164329996").send(botstartinfo)
   let m2 = await bot.channels.get("507175036092940299").send(botstartinfo)
   let m3 = await bot.channels.get("518054671286534190").send(botstartinfo)
