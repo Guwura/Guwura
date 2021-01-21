d695f8b6b8201efc43d2cd823d6ff7437021f743
WeiKuOuO <43096905+WeiKuOuO@users.noreply.github.com>
2021-01-22 00:40:41 +0800

    Create help.js

---
+++ commands/help.js
@@ -0,0 +1,9 @@
+const Eris = require("eris");
+
+module.exports.run = (bot, msg, args, tools) => {
+    bot.createMessage(msg.channel.id, "i dont rly have a command list rn qwq");
+}
+
+module.exports.help = {
+    name: "test"
+} 
